c37d277ff2835234128c0798f5600db98e107f81
Robinnsson Cifuentes Ramírez <59662419+C2R-C@users.noreply.github.com>
2023-07-13 20:39:20 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MVVM Project"
+include ("app",
+":model",
+":datasource",
+":data",
+":domain",
+)
